c18ec740d92f72ed7a8cb4c2d6ee3fa1770f5ed9
he.wenyao <he.wenyao@nint.com>
2023-10-24 13:05:52 +0800

    feat: 名片分享服务

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-headlessui",
+    "nuxt-icon",
+  ],
+
+  headlessui: {
+    prefix: "Headless",
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+  content: [
+    `components/**/*.{vue,js}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `composables/**/*.{js,ts}`,
+    `plugins/**/*.{js,ts}`,
+    `App.{js,ts,vue}`,
+    `app.{js,ts,vue}`,
+  ],
+}
+
