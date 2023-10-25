026fa988132643c3dd973d1c244a8a48d4d6f266
= <=>
2023-10-26 00:10:24 +0800

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app:{
+    head: {
+      title: '哗啦分享',
+      htmlAttrs: {
+        lang: 'zh-CN',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
   devtools: { enabled: true },
   modules: [
     "@nuxtjs/tailwindcss",
@@ -15,7 +29,7 @@ export default defineNuxtConfig({
         wght: [100, 400],
         ital: [100],
       },
-      subsets:['latin']
+      subsets: [0],
     },
   },
   headlessui: {
