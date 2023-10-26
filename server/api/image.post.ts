ea7f62980218a629b5434fd5b3476f70e56860c0
he.wenyao <he.wenyao@nint.com>
2023-10-26 19:13:01 +0800

    fix

--- server/api/image.post.ts
+++ server/api/image.post.ts
@@ -1,33 +1,41 @@
+import { Octokit } from "@octokit/core";
+import { v4 as uuidv4 } from "uuid";
+import dayjs from "dayjs";
+
 export default defineEventHandler(async (event) => {
-  const [file] = await readMultipartFormData(event);
-  console.log(file);
+  const files = await readMultipartFormData(event);
+  if (!files) {
+    return {
+      notFound: true,
+    };
+  }
+  console.log(files[0]);
+  const [ file ] = files;
+  const [, suffix] = file.name.split(".");
+  const conf = {
+    owner: "hualafun",
+    repo: "storage",
+    token: "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",
+  };
 
-  return { dd: 1 };
-
-  // const conf = {
-  //   owner: "hualafun",
-  //   repo: "storage",
-  //   token: "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",
-  // };
-
-  // const octokit = new Octokit({
-  //   auth: conf.token,
-  // });
-  // const { owner, repo } = conf;
-  // const filename = `${uuidv4()}.${suffix}`;
-  // const uploadPath = `/repos/${owner}/${repo}/contents/${path}/${filename}`;
-  // await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
-  //   owner: "OWNER",
-  //   repo: "REPO",
-  //   path: "PATH",
-  //   message: "my commit message",
-  //   committer: {
-  //     name: "Monalisa Octocat",
-  //     email: "hualafun@gmail.com",
-  //   },
-  //   content: "bXkgbmV3IGZpbGUgY29udGVudHM=",
-  //   headers: {
-  //     "X-GitHub-Api-Version": "2022-11-28",
-  //   },
-  // });
+  const octokit = new Octokit({
+    auth: conf.token,
+  });
+  const { owner, repo } = conf;
+  const filename = `${uuidv4()}.${suffix}`;
+  const uploadPath = `/repos/${owner}/${repo}/contents/${path}/${filename}`;
+  await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+    owner: "OWNER",
+    repo: "REPO",
+    path: "PATH",
+    message: "my commit message",
+    committer: {
+      name: "Monalisa Octocat",
+      email: "hualafun@gmail.com",
+    },
+    content: "bXkgbmV3IGZpbGUgY29udGVudHM=",
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
 });
