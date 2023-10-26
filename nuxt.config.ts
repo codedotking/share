335039aa974f1bf032757e29e9416184aabd9410
he.wenyao <he.wenyao@nint.com>
2023-10-26 11:59:27 +0800

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app:{
+  devtools: { enabled: true },
+
+  app: {
     head: {
-      title: '哗啦分享',
+      title: "哗啦分享",
       htmlAttrs: {
-        lang: 'zh-CN',
+        lang: "zh-CN",
       },
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' },
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "" },
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
   },
-  devtools: { enabled: true },
   modules: [
-    "@nuxtjs/tailwindcss",
     "nuxt-headlessui",
     "nuxt-icon",
     "@nuxt/image",
-    "@nuxtjs/google-fonts",
+    "@nuxt/ui",
   ],
-  googleFonts: {
-    families: {
-      Poppins: true,
-      Raleway: {
-        wght: [100, 400],
-        ital: [100],
-      },
-      subsets: [0],
-    },
-  },
   headlessui: {
     prefix: "Headless",
   },
