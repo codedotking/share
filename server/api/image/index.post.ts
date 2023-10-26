a391028c31a6385fd39668eae82327ac26791ba7
= <=>
2023-10-27 00:45:37 +0800

    fix

--- server/api/image/index.post.ts
+++ server/api/image/index.post.ts
@@ -1,38 +1,34 @@
 import { Octokit } from "@octokit/core";
 import { v4 as uuidv4 } from "uuid";
 import dayjs from "dayjs";
+import { readFiles } from "h3-formidable";
+import fs from "fs";
 
 export default defineEventHandler(async (event) => {
-  const files = await readMultipartFormData(event);
-  
-  if (!files || files.length === 0) {
+  const {
+    fields: { name = "" },
+    files: { file = [] } = {},
+    form,
+  } = await readFiles(event);
+  if (file.length === 0) {
     throw createError({
       statusCode: 400,
-      statusMessage: "No files were uploaded",
+      statusMessage: "没有文件上传",
     });
   }
+  const [_file] = file;
+  const { mimetype = "", filepath } = _file;
+  const type = mimetype || "";
+  if (!["image/png", "image/jpg"].includes(type)) {
+    return {
+      code: 400,
+      msg: "只支持 png 和 jpg",
+    };
+  }
+  const buffer = fs.readFileSync(filepath);
+  const [, suffix] = type.split("/");
+  const base64 = buffer.toString("base64");
 
-  const uploaded = files.map((item) => ({
-    contentType: item.type,
-    filename: decodeURIComponent(item.filename!),
-    name: item.name,
-    size: item.data.length,
-  }));
-
-  // const [file] = files;
-  // const { type = "", data: buffer } = file;
-  // if (!["image/png", "image/jpg"].includes(type)) {
-  //   return {
-  //     code: 400,
-  //     msg: "只支持 png 和 jpg",
-  //   };
-  // }
-
-  // const [, suffix] = type.split("/");
-  return {
-    files,
-    // suffix,
-  };
   const conf = {
     owner: "hualafun",
     repo: "storage",
@@ -44,20 +40,29 @@ export default defineEventHandler(async (event) => {
   });
   const { owner, repo } = conf;
   const filename = `${uuidv4()}.${suffix}`;
-  const uploadPath = `/repos/${owner}/${repo}/contents/${path}/${filename}`;
-  // upload
-  await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
-    owner: "OWNER",
-    repo: "REPO",
-    path: "PATH",
-    message: "my commit message",
-    committer: {
-      name: "Monalisa Octocat",
-      email: "hualafun@gmail.com",
-    },
-    content: buffer.toString("base64"),
-    headers: {
-      "X-GitHub-Api-Version": "2022-11-28",
-    },
-  });
+  const uploadPath = `/repos/${owner}/${repo}/contents/${name}/${filename}`;
+  try {
+    // upload
+    await octokit.request(`PUT ${uploadPath}`, {
+      owner,
+      repo,
+      path: `${name}`,
+      message: uploadPath,
+      committer: {
+        name: "Monalisa Octocat",
+        email: "hualafun@gmail.com",
+      },
+      content: base64,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+    return {
+      uploadPath,
+    };
+  } catch (e) {
+    return {
+      error: JSON.stringify(e),
+    };
+  }
 });
